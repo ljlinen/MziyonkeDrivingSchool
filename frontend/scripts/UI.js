8f07b4e3e9e885b1e85652fe0f00d4b9ced153f3
unknown <Siphomoloto@gmail.com>
2024-04-21 16:12:42 -0700

    first commit for mziyonkedrivingschool repo

---
+++ frontend/scripts/UI.js
@@ -0,0 +1,134 @@
+//When a card is clicked, it should move into view
+
+import { apiFetch } from "./api.js";
+
+const servCards = document.querySelectorAll('.servCard');
+const servContent = document.querySelector('.servContent');
+
+servCards.forEach((card) => {
+  card.addEventListener('click', () => {
+    //card.style.transform = 'translateX(30vw)';
+    servContent.scrollWidth = '40px'
+  })
+});
+
+//When hamburger menu is clicked, it should transform into a 'x'
+//when clicked, menu contents should showup
+
+const menu = document.querySelector('.menu');
+const menuCancel = document.querySelector('.menu-cancel');
+const menuContent = document.querySelector('.menu-content');
+
+let clicked = false;
+
+ menu.addEventListener('click', () => {
+  
+  if(!clicked) {
+  menuContent.style.zIndex = '5';
+  clicked = true;
+  } else {
+  menuContent.style.zIndex = '-5';
+  clicked = false;
+  }
+});
+
+menuCancel.addEventListener('click', () => {
+  
+  if(!clicked) {
+  menuContent.style.zIndex = '5';
+  clicked = true;
+  } else {
+  menuContent.style.zIndex = '-5';
+  clicked = false;
+  }
+}); 
+
+function testFunction() {
+  alert("Works!");
+}
+
+    //When any link is clicked, it should scroll to the requested view
+
+      function scrollToElement(elementClass) {
+      const elementToScrollToView = document.querySelector(elementClass);
+      const elementStyle = getComputedStyle(elementToScrollToView);
+    
+        if(elementStyle.display == 'none') {
+           elementToScrollToView.style.display = 'flex';
+        }
+           elementToScrollToView.scrollIntoView({scrollBehavior:"smooth"});
+    }
+
+
+    //when card apply button clicked, select the learners on the form by defualt
+    
+    const applyFromCard = (license) => {
+      if(typeof license === 'number')
+      document.querySelector('#license').selectedIndex = license;
+      scrollToElement('.section-application');
+    }
+
+  //Gets input valuesand validates
+
+  function inputValue(inputID) {
+
+    var input = document.querySelector(`#${inputID}`);
+
+    if (!input.value) {
+      input.style.border = '3.2px solid #e35252';
+      input.placeholder = 'provide info';
+    } else {
+      input.style.border = 'unset';
+      return input.value;
+    }
+  }
+
+//submits the form info when form button is clicked
+//recieves a json object as response
+
+document.getElementById('application-submit').addEventListener('click', async() => {
+  const gender = document.querySelector('input[name="gender"]:checked');
+  const genderLabel = document.querySelector('.gender-label');
+
+  const objPutData = {
+    id: '00',
+    name: inputValue('name'),
+    lastname: inputValue('lastname'),
+    gender: 'male',
+    email: inputValue('email'),
+    phone: inputValue('phone'),
+    license: 'learners'
+  }
+  
+  for(let [key, value] of Object.entries(objPutData)) {
+    if(!(value.length > 0) || value == undefined) {
+      return
+    }
+  }
+  if (!gender) { 
+    genderLabel.style.color = '#e35252';
+    return
+  } else {
+    genderLabel.style.color = 'unset';
+  }
+  
+    await apiFetch('PUT', 'application', objPutData)
+    .then((res) => {
+
+      document.querySelector('.application').style.display = 'none';
+      document.querySelector('.section-application').innerText = `Application Submitted`;
+
+      if(res.success) {
+      alert(res.success);
+      } else {
+      alert(res)
+      }
+  }).catch((error) => {
+    if(error.error) {
+      alert(error.error);
+      } else {
+      alert(error)
+      }     
+  })
+
+})
---
+++ frontend/scripts/api.js
@@ -0,0 +1,51 @@
+//Reusable fetch request
+let envport = '';
+let envhost = '';
+
+await fetch('./env.json')
+   .then(response => {
+     if (!response.ok) {
+       throw new Error('failed loading env json');
+     }
+     return response.json();
+   })
+   .then(data => {
+     envport = data.port;
+     envhost = data.host;
+   })
+   .catch(error => {
+     console.error('Error:', error);
+   });
+
+zasync function apiFetch(fetchType, endpoint, params) {
+ 
+    const options = {
+        method: `${fetchType}`,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params),
+    }
+
+    if (params instanceof Object) {
+        console.log('params is object ' + JSON.stringify(params));
+        //options.body = JSON.stringify(params);
+    } else {
+        console.log('params not object ' + JSON.stringify(params));
+        throw new Error('params is not an object')
+    }
+
+    try {
+        const response = await fetch('https//localhost:3000/' + `${endpoint}`, options);
+        if (!response.ok) {
+            throw await response.json();
+        }
+        
+        console.log('request was successful');        
+        const responseBody = await response.json(); // Parse response body as JSON
+        return responseBody;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
